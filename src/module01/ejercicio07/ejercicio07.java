01b19378201581854227daf8224b475d50a1cb24
davidpyxl <dcastellar@pyxl.com>
2022-09-17 00:08:47 -0500

    termina ejercio numero 7

---
+++ src/module01/ejercicio07/ejercicio07.java
@@ -0,0 +1,13 @@
+package module01.ejercicio07;
+
+public class ejercicio07 {
+   public static void main(String[] args) {
+      int n1 = 10;
+      int n2 = 20;
+      int n3 = 30;
+
+      System.out.printf("a) El total de n1 + n2 + n3 es %d%n", (n1 + n2 + n3));
+      System.out.printf("b) El promedio de n1 + n2 + n3 es %d%n", (n1 + n2 + n3) / 3);
+      System.out.printf("c) El resto de n2 + n1 es %d%n", (n2 % n1));
+   }
+}
---
+++ src/module01/ejercicio07/readme.md
@@ -0,0 +1,10 @@
+### Ejercicio 7
+
+1. Resolver los enunciados desarrollando el código.
+2. Dados n1=10, n2=20 y n3=30, informar:
+
+   a) El total.
+
+   b) El promedio.
+
+   c) El resto entre n2 y n1.
