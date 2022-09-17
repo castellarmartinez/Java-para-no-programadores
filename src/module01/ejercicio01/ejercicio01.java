8e34fd1d7fbaa066a7536ee1f7fe0f7104bd451c
davidpyxl <dcastellar@pyxl.com>
2022-09-16 23:14:28 -0500

    first commit

---
+++ src/module01/ejercicio01/ejercicio01.java
@@ -0,0 +1,20 @@
+package module01.ejercicio01;
+
+import java.util.Scanner;
+
+public class ejercicio01 {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+      System.out.print("Ingresa un número entero de horas y te mostraré " +
+              "el equivalente en semanas, días y horas: ");
+      int horasIngresadas = input.nextInt();
+
+      int semanas = horasIngresadas / 168;
+      int dias = (horasIngresadas % 168) / 24;
+      int horas = (horasIngresadas % 168) % 24;
+
+      System.out.printf("%d horas equivalen a %d semanas, %d días y %d horas.",
+              horasIngresadas, semanas, dias, horas);
+   }
+}
---
+++ src/module01/ejercicio01/readme.md
@@ -0,0 +1,3 @@
+### Ejercicio 1
+
+Crear un programa en Java que, dado un número entero de horas, muestre el equivalente en semanas, días y horas.
