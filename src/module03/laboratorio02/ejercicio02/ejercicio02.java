cd02f590408b5eb807612af06e0497d3339b1061
David Castellar Martinez <castellardav@gmail.com>
2022-10-14 23:22:26 -0500

    termina ejercicio 2 del laboratorio 2 del modulo 3

--- src/module03/laboratorio02/ejercicio02/ejercicio02.java
+++ src/module03/laboratorio02/ejercicio02/ejercicio02.java
@@ -1,2 +1,28 @@
-package module03.laboratorio02.ejercicio02;public class ejercicio02 {
+package module03.laboratorio02.ejercicio02;
+
+import java.util.Scanner;
+
+public class ejercicio02 {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int temperatura;
+
+      System.out.print("Ingresa la temperatura actual: ");
+      temperatura = input.nextInt();
+
+      hacerAccion(temperatura);
+   }
+
+   private static void hacerAccion(int temperatura) {
+      if (temperatura > 25) {
+         System.out.println("Puede caminar y tomar sol\n");
+      } else if (temperatura > 15) {
+         System.out.println("Puede caminar\n");
+      } else if (temperatura > 10) {
+         System.out.println("Puede caminar con campera\n");
+      } else if (temperatura >= 0) {
+         System.out.println("Puede quedarse en casa o salir con paraguas y campera");
+         System.out.println("Puede esquiar\n");
+      }
+   }
 }
---
+++ src/module03/laboratorio02/ejercicio02/readme.md
@@ -0,0 +1,13 @@
+### Ejercicio 2
+
+Dada la siguiente tabla del tiempo, hacer un programa que indique qué puede hacer una
+persona con dicho pronóstico:
+
+
+| Temperatura    | Tiempo  | Sugerencia                                      | 
+|----------------|---------|-------------------------------------------------|
+| > 25°          | Soleado | Caminar y tomar sol                             |
+| > 15° y <= 25° | Soleado | Caminar                                         |
+| <= 15°         | Soleado | Caminar con campera                             |
+| < 10°          | Lluvia  | Quedarse en casa o salir con paraguas y campera |
+| < 10°          | Nieve   | Esquiar                                         |
