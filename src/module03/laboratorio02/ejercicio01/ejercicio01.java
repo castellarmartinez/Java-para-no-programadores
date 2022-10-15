76838a3d09b7d1bbfe035632777a3ca0700bea86
David Castellar Martinez <castellardav@gmail.com>
2022-10-14 23:06:38 -0500

    termina ejercicios del laboratorio 2 del modulo 3

--- src/module03/laboratorio02/ejercicio01/ejercicio01.java
+++ src/module03/laboratorio02/ejercicio01/ejercicio01.java
@@ -1,2 +1,24 @@
-package module03.laboratorio02.ejercicio01;public class ejercicio01 {
+package module03.laboratorio02.ejercicio01;
+
+import java.util.Scanner;
+
+public class ejercicio01 {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      String usuario = "deivic";
+      String contrasena = "David007";
+      String usuarioIngresado;
+      String contrasenaIngresada;
+
+      System.out.print("Ingresa el usuario: ");
+      usuarioIngresado = input.nextLine();
+      System.out.print("Ingresa la contraseña: ");
+      contrasenaIngresada = input.nextLine();
+
+      if (usuario.equals(usuarioIngresado) && contrasena.equals(contrasenaIngresada)) {
+         System.out.println("Bienvenido al Sistema");
+      } else {
+         System.out.println("Credenciales incorrectas");
+      }
+   }
 }
---
+++ src/module03/laboratorio02/ejercicio01/readme.md
@@ -0,0 +1,6 @@
+### Ejercicio 1
+
+Crear un programa que simule un inicio de sesión con dos cadenas de caracteres (usuario y contraseña).
+Luego, mostrar dos tipos de mensajes en pantalla:
+
+“Bienvenido al Sistema” y “Credenciales incorrectas” en caso de que el ingreso sea incorrecto
