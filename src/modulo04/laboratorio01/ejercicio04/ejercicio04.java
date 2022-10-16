6653825f222a0127dc90e0d5fff3d36725af8964
David Castellar Martinez <castellardav@gmail.com>
2022-10-16 14:01:47 -0500

    termina ejercicio 4 del laboratorio 1 del modulo 4

---
+++ src/modulo04/laboratorio01/ejercicio04/ejercicio04.java
@@ -0,0 +1,13 @@
+package modulo04.laboratorio01.ejercicio04;
+
+public class ejercicio04 {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int numero = 1; numero <= 10; numero++) {
+            suma += numero;
+        }
+
+        System.out.println("La suma de los números del 1-10 es: " + suma);
+    }
+}
