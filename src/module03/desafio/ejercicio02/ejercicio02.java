194acfcdcf5fd09e486207d2b5cfc3937ac3fa3a
David Castellar Martinez <castellardav@gmail.com>
2022-10-16 12:44:56 -0500

    termina ejericio 2 del desafio del modulo 3

--- src/module03/desafio/ejercicio02/ejercicio02.java
+++ src/module03/desafio/ejercicio02/ejercicio02.java
@@ -1,4 +1,42 @@
 package module03.desafio.ejercicio02;
 
+import java.util.Scanner;
+
 public class ejercicio02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int numero3;
+        int producto = 1;
+        boolean hayNumerosPositivos = false;
+
+        System.out.print("Ingresa el numero 1: ");
+        numero1 = input.nextInt();
+        System.out.print("Ingresa el numero 2: ");
+        numero2 = input.nextInt();
+        System.out.print("Ingresa el numero 3: ");
+        numero3 = input.nextInt();
+
+        if (numero1 > 0) {
+            producto *= numero1;
+            hayNumerosPositivos = true;
+        }
+
+        if (numero2 > 0) {
+            producto *= numero2;
+            hayNumerosPositivos = true;
+        }
+
+        if (numero3 > 0) {
+            producto *= numero3;
+            hayNumerosPositivos = true;
+        }
+
+        if (hayNumerosPositivos) {
+            System.out.println("El producto de los números positivos es: " + producto);
+        } else {
+            System.out.println("El producto de los números positivos es: no se ingresaron números positivos");
+        }
+    }
 }
---
+++ src/module03/desafio/ejercicio02/readme.md
@@ -0,0 +1,28 @@
+### Ejercicio 2
+
+A partir del siguiente código:
+
+int a = 10, b=-2, c=5;\
+hay 2 números positivos y 1 negativo
+
+Informar la multiplicación de los dos
+números positivos.
+
+#### Pseudocódigo
+
+    PROGRAMA 2
+    LEER a,b,c
+    SI a>0 y b>0
+    ENTONCES
+    INFORMAR a*b
+    FIN SI
+    SI a>0 y c>0
+    ENTONCES
+    INFORMAR a*c
+    FIN SI
+    SI b>0 y c>0
+    ENTONCES
+    INFORMAR a*b
+    FIN SI
+    FIN PROGRAMA
+
